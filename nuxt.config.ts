a0f9dff47338cccbc57666de148603f64cd675c2
realabdullah <abdulodesanmi@gmail.com>
2024-01-20 14:22:51 +0100

    chore: added fonts and custom styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: ["~/assets/scss/main.scss"],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
+						@import "@/assets/scss/abstract/_variables.scss"; 
+						@import "@/assets/scss/abstract/_mixins.scss";
+					`,
+                },
+            },
+        },
+    },
     devtools: { enabled: true },
 });
